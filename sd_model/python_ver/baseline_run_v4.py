e4f5981eae23ffbc28612980433a68cee1d93d3a
JuanRobledo12 <jroblar98@gmail.com>
2025-06-18 22:33:12 -0600

    Remove commented-out plotting code from baseline run script

--- sd_model/python_ver/baseline_run_v4.py
+++ sd_model/python_ver/baseline_run_v4.py
@@ -51,26 +51,6 @@ for time in time_range:
 # Convert to DataFrame
 df = pd.DataFrame(results)
 
-# # Plot all columns except 'time'
-# plot_columns = [col for col in df.columns if col != "time"]
-# n_cols = 2
-# n_rows = int(np.ceil(len(plot_columns) / n_cols))
-
-# fig, axs = plt.subplots(n_rows, n_cols, figsize=(7 * n_cols, 4 * n_rows))
-# axs = axs.flatten()
-
-# for i, col in enumerate(plot_columns):
-#     axs[i].plot(df["time"], df[col])
-#     axs[i].set_title(f"{col.replace('_', ' ').title()} Over Time")
-
-# # Hide any unused subplots
-# for j in range(i + 1, len(axs)):
-#     fig.delaxes(axs[j])
-
-# plt.tight_layout()
-# plt.show()
-# plt.savefig(os.path.join(FIGURES_DIR_PATH, "simulation_results.png"))
-
 # Save results to CSV
 output_file_name = f"baseline_sim_results_{config_file_name}.csv"
 output_file_path = os.path.join(BASELINE_SIM_RESULTS_DIR_PATH, output_file_name)
